ac7ca9e276ae46690ac1877cf30660feb716cb5b
romainavinenc <romain.avinenc@gmail.com>
2025-08-19 23:17:06 +0200

    Fix

--- src/components/ProtectRoute.tsx
+++ src/components/ProtectRoute.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom';
 
-export const ProtectedRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
+export const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const token = localStorage.getItem('token');
     if (!token) return <Navigate to="/auth" replace />;
     return children;
