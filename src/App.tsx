7c1d669a415e5fc86f0904bd25898d3533e004b1
romainavinenc <romain.avinenc@gmail.com>
2025-08-19 18:17:26 +0200

    Securisation des routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,38 @@ import {SessionPage} from "./pages/SessionPage.tsx";
 import {SessionDetailPage} from "./pages/SessionDetailPage.tsx";
 import {TargetPageWrapper} from "./pages/TargetPageWrapper.tsx";
 import {ProfilePage} from "./pages/ProfilePage.tsx";
+import {ProtectedRoute} from "./components/ProtectRoute.tsx";
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/auth" element={<AuthPage />} />
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/sessions" element={<SessionPage />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/session/:id" element={<SessionDetailPage />} />
-                <Route path="/sessions/:sessionId" element={<TargetPageWrapper/>} />
+                <Route path="/home" element={
+                    <ProtectedRoute>
+                        <HomePage />
+                    </ProtectedRoute>}
+                />
+                <Route path="/sessions" element={
+                    <ProtectedRoute>
+                        <SessionPage />
+                    </ProtectedRoute>}
+                />
+                <Route path="/profile" element={
+                    <ProtectedRoute>
+                        <ProfilePage />
+                    </ProtectedRoute>}
+                />
+                <Route path="/session/:id" element={
+                    <ProtectedRoute>
+                        <SessionDetailPage />
+                    </ProtectedRoute>}
+                />
+                <Route path="/sessions/:sessionId" element={
+                    <ProtectedRoute>
+                        <TargetPageWrapper/>
+                    </ProtectedRoute>}
+                />
             </Routes>
         </Router>
     );
